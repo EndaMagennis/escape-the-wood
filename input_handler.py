24bea602f8653cbf99a7756ea71551020b186d3e
EndaMagennis <>
2023-08-18 16:25:56 +0000

    Create class to manage user inputs

---
+++ input_handler.py
@@ -0,0 +1,23 @@
+class InputHandler():
+    """
+    Handles user inputs and carries out relevant methods
+    """
+    def __init__(self):
+        pass
+
+    def register_user_inputs(self):
+        game = True
+        actions = [
+            "move", "use", "pick up", "attack",
+            "look", "equip", "fight", "exit"
+        ]
+        while game:
+
+            user_action = input("What would you like to do?\n")
+
+            for a in actions:
+                if str(a) == str(user_action):
+                    if str(user_action) == str(actions[-1]):
+                        print("Exiting")
+                        game = False
+                    print(user_action)
