1f6e5dc811c786359e9228bfabbe7e0175760d98
EndaMagennis <>
2023-08-18 14:52:35 +0000

    Import Player class

--- scripts/run.py
+++ scripts/run.py
@@ -1,7 +1,8 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-import player
+from player import Player
+
 intro = """\nYou have awoken in a ramshackel abode on the outskirts of a town.
 You have no recollection of who you are or how you've gotten here.
 A voice calls out to you, seemingly from nowhere.\n
@@ -10,13 +11,12 @@ That you have awoken is a testament in itself.
 Now piece yourself together. Who are you?'\n
 Your head begins swimming; the past rushing to meet the present.
 You suddenely remember yourself.\n"""
-
-current_player = player.Player("", [], (0, 0), {}, 1)
+player = Player("", [], (0, 0), [], 1)
 
 
 def main():
     print(intro)
-    current_player.name_player()
+    player.name_player()
 
 
 main()
