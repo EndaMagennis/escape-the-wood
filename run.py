7fe6ffa95c5b8cee9e6dacdb5e6efb4bb837cfaa
EndaMagennis <>
2023-08-18 16:28:58 +0000

    Import input_handler and add to main function

--- run.py
+++ run.py
@@ -2,6 +2,9 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import player
+import input_handler
+
+
 intro = """\nYou have awoken in a ramshackel abode on the outskirts of a town.
 You have no recollection of who you are or how you've gotten here.
 A voice calls out to you, seemingly from nowhere.\n
@@ -12,11 +15,13 @@ Your head begins swimming; the past rushing to meet the present.
 You suddenely remember yourself.\n"""
 
 current_player = player.Player("", [], (0, 0), {}, 1)
+iph = input_handler.InputHandler()
 
 
 def main():
     print(intro)
     current_player.name_player()
+    iph.register_user_inputs()
 
 
 main()
