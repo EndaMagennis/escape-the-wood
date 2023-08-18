afa1fa9dd509e93a4d4d4757a57d2baf74bad03d
EndaMagennis <>
2023-08-18 11:51:57 +0000

    Create get_user_input function and main function

--- run.py
+++ run.py
@@ -1,4 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-print("Hello World")
+
+def get_user_input():
+    """
+    Ask user to input their name and print a
+    welcome message.
+    """
+    player_name = input("Enter your name:\n")
+    print(f"Welcome, {player_name}. Your journey begins here.")
+
+
+def main():
+    get_user_input()
+
+
+main()
