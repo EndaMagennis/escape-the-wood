f57fb0867366fb41ecea91f7b7522b4418f2e1a1
EndaMagennis <>
2023-08-20 19:31:47 +0000

    Create ditionary of findable objects and create return_item function

---
+++ item.py
@@ -0,0 +1,24 @@
+FINDABLE_ITEMS = {
+    # dictionary entries for types of items which can be found in the game
+    "weapons": {
+        "broken sword": 10,
+        "shortsword": 30,
+        "dagger": 25,
+        "mace": 35,
+        },
+    "keys": {
+        "cottage key": "key to the cottage",
+        "golden key": "key with a golden hue",
+        "rusted key": "an old key, brittle and rough"
+    },
+    "notes": {
+        "note": "I've found it, finally",
+    }
+}
+
+
+def return_item(request):
+    for id, items in FINDABLE_ITEMS.items():
+        for key, value in items.items():
+            if request == key:
+                return {key: value}
