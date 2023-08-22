0a5f4b5330996876339f85456a43998ada32e365
EndaMagennis <>
2023-08-22 12:07:07 +0000

    Add docstring descriptions and update Class declaration

--- room.py
+++ room.py
@@ -2,20 +2,30 @@ import item
 
 
 class Room:
-
-    def __init__(self, name, description, inventory, searchable_area):
+    """
+    Creates an instance of the Room class.
+    """
+    def __init__(self, name, description, inventory, searchable_areas, map_location):
         self.name = name
         self.description = description
         self.inventory = inventory
-        self.searchable_area = searchable_area
+        self.searchable_areas = searchable_areas
+        self.map_location = map_location
 
     def describe_room(self):
-        
+        """
+        Describes the instance of a room.
+        """
         print(self.description)
 
 
+# creating a 'cottage' room instance
 cottage = Room(
-    "Cottage", """A small, rustic, thach rooved house.
-    It's barren interior only hinting at a life which once resided within""",
-    item.return_item("cottage key"), {}
+    "The Cottage", f"""A small, rustic, thach rooved house.
+    It's barren interior only hinting at a life which once resided within.
+    In the western corner of the room you see a bookshelf.
+    in the center of the room there is a large bed.
+    Next to the bed, there is a cabinet
+    """,
+    item.return_item("cottage key"), ["bookshelf", "cabinet", "bed"], (0, 0)
     )
