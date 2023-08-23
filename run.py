5c09538af07dc678d9f0120812699cc5bf20eb2e
Enda Magennis <101476399+RopesMcMeme@users.noreply.github.com>
2023-08-23 20:40:01 +0100

    Add and update function calls

--- run.py
+++ run.py
@@ -1,32 +1,29 @@
 import player
-import item
-import room
+import map
 
 # variable to store the intro of the game
-intro = """\nYou have awoken in a ramshackel abode on the outskirts of a town.
-You have no recollection of who you are or how you've gotten here.
-A voice calls out to you, seemingly from nowhere.\n
-'Ho, there weary traveller, you are here for the Challenge of Kings.
-That you have awoken is a testament in itself.
-Now piece yourself together. Who are you?'\n
-Your head begins swimming; the past rushing to meet the present.
-You suddenely remember yourself.\n"""
+intro = f"""
+    You have awoken in a ramshackel abode on the outskirts of a town.
+    You have no recollection of who you are or how you've gotten here.
+    A voice calls out to you, seemingly from nowhere.
+    
+        'Ho, there weary traveller, you are here for the Challenge of Kings.
+    That you have awoken is a testament in itself.
+    Now piece yourself together. Who are you?'
 
-# instantiating the rooms of the game world
-cottage = room.cottage
-woods = room.woods
-dark_woods = room.dark_woods
-church = room.chruch
-village_path = room.village_path
+    Your head begins swimming; the past rushing to meet the present.
+    You suddenely remember yourself."""
 
 # instantiating the player
-current_player = player.Player("", [], dark_woods, [])
+current_player = player.Player("", [], map.generate_room_from_name("The Cottage"), [])
 
 
 def main():
     print(intro)
-    print(cottage.linked_rooms)
-    current_player.search(dark_woods, dark_woods.searchable_areas)
+    map.set_item_locations_for_each_room()
+    current_player.name_player()
+    current_player.current_room.describe_room()
+    current_player.register_user_inputs(current_player.current_room)
 
 
 main()
