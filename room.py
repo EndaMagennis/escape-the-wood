98cb74737c0864aeee9a09cf8d94a404b446b6f8
Enda Magennis <101476399+RopesMcMeme@users.noreply.github.com>
2023-08-24 18:02:28 +0100

    Add class booleans and create describe event method

--- room.py
+++ room.py
@@ -9,13 +9,14 @@ class Room:
     and paths to other rooms
     """
     def __init__(
-                self, name, description, path_descriptions, inventory,
+                self, name, description, path_descriptions, event_description, inventory,
                 searchable_areas, item_location, linked_rooms, required_item, 
-                item_found, has_been_visited
+                item_found, has_been_visited, has_event, has_encounter
                 ):
         self.name = name
         self.description = description
         self.path_descriptions = path_descriptions
+        self.event_description = event_description
         self.inventory = inventory
         self.searchable_areas = searchable_areas
         self.item_location = item_location
@@ -23,6 +24,8 @@ class Room:
         self.required_item = required_item
         self.item_found = item_found
         self.has_been_visited = has_been_visited
+        self.has_event = has_event
+        self.has_encounter = has_encounter
 
     def type_descritions(self, description):
         """
@@ -44,6 +47,7 @@ class Room:
         """
         print(self.name)
         self.type_descritions(self.description)
+        self.describe_event()
 
     def describe_paths(self):
         """
@@ -51,6 +55,12 @@ class Room:
         """
         print(self.name)
         self.type_descritions(self.path_descriptions)
+
+    def describe_event(self):
+        if self.has_event:
+            self.type_descritions(self.event_description)
+        else:
+            return
 
     def choose_random_item_location(self, searchable_areas):
         """
