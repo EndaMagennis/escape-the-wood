e623aa55be5950ff63f163c9de4378c7268ed899
Enda Magennis <101476399+RopesMcMeme@users.noreply.github.com>
2023-08-25 19:30:16 +0100

    Refactor search method to allow for partial string match

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import map
+import game_environment
 from time import sleep
-import os
 
 class Player:
     """
@@ -24,10 +24,10 @@ class Player:
         """
         try:
             # Check if user enters exit at any input phase
-            if input == "exit":
-                return
+            if input == "exit" or input == "EXIT":
+                return input
             # Check that only numbers and letter and spaces are used
-            if not input.isalnum() and input.isspace():
+            if not input.isalnum() and not input.isspace():
                 raise TypeError
             # Control the length of the input
             elif len(input) < 2 or len(input) > 22:
@@ -42,65 +42,73 @@ class Player:
             return False
         return True
 
-    def register_user_inputs(self, room):
+    def register_user_inputs(self):
         """ Records user inputs and calls the relevant methods """
-        # Setting game to true for a continuous loop
+        # Setting self.alive to true for a continuous loop
         self.alive = True
         room = self.current_room
-        areas = room.searchable_areas
-        # a list of possible actions
+        # A list of possible actions
         actions = [
             "SEARCH", "MOVE", "CHECK", 
             "LOOK", "USE", "DESCRIBE", "EXIT", 
         ]
-
+        # Running a continuous loop
         while self.alive:
             print(f"You can do any of the following things:\n")
             print(*actions, sep = ", ")
             print()
-            user_action = input("What would you like to do?\n")
 
+            # Promting user for input
+            user_action = input("What would you like to do?\n")
+            # Validatong user input
             if self.validate_input(
                 user_action, 
-                "That is not a valid action. Try again\n", 
-                "That is not a valid action. Try again\n") == False:
+                f"Not quite sure what you mean by {user_action}, use your words!\n", 
+                "Give me at least the first two letters of your intention, for Pete's sake!\n") == False:
                     continue
             # Removing case sensitivity and checking for valid actions
             user_action = user_action.upper()
-            if user_action in actions:
-                if user_action == actions[0]:
-                    # Clearing the terminal
-                    os.system('clear')
-                    # Calling the search method
-                    self.search(room, areas)
-                elif user_action == actions[1]:
-                    # Clearing the terminal
-                    os.system('clear')
-                    # Calling the move_to_room method
-                    self.move_to_room(room, "")
-                elif user_action == actions[2]:
-                    # Clearing the terminal
-                    os.system('clear')
-                    # Calling the check_inventory method
-                    self.check_inventory()
-                elif user_action == actions[3]:
-                    # Clearing the terminal
-                    os.system('clear')
-                    # Calling the look method
-                    self.look(self.current_room)
-                elif user_action == actions[4]:
-                    # Clearing the terminal
-                    os.system('clear')
-                    # Calling the use_item method
-                    self.use_item(self.inventory)
-                elif user_action == actions[5]:
-                    # Clearing the terminal
-                    os.system('clear')
-                    # Calling the describe_room method
-                    self.current_room.describe_room()
-                elif user_action == actions[6]:
-                    print("Exiting")
-                    self.alive = False
+            # Checking if user_action partially matches the action
+            if user_action in actions[0]:
+                # Clearing the terminal
+                game_environment.clear()
+                # Calling the search method
+                self.search()
+
+            elif user_action in actions[1]:
+                # Clearing the terminal
+                game_environment.clear()
+                # Calling the move_to_room method
+                self.move_to_room(room, "")
+
+            elif user_action in actions[2]:
+                # Clearing the terminal
+                game_environment.clear()
+                # Calling the check_inventory method
+                self.check_inventory()
+
+            elif user_action in actions[3]:
+                # Clearing the terminal
+                game_environment.clear()
+                # Calling the look method
+                self.look()
+
+            elif user_action in actions[4]:
+                # Clearing the terminal
+                game_environment.clear()
+                # Calling the use_item method
+                self.use_item(self.inventory)
+
+            elif user_action in actions[5]:
+                # Clearing the terminal
+                game_environment.clear()
+                # Calling the describe_room method
+                self.current_room.describe_room()
+
+            elif user_action in actions[6]:
+                print("Exiting")
+                # Ending the loop
+                self.alive = False
             else:
                 print("That is not a valid choice. Try again.")
 
@@ -126,48 +134,63 @@ class Player:
             print(f"Hello, {player_name}. Your journey begins in: ")
             return player_name
 
-    def search(self, room, areas):
+    def search(self):
         """ Prompts user to search areas based on current room."""
+        # Set room variable to current room
         room = self.current_room
+        # Set possible areas to this room's searchable areas
+        possible_areas = room.searchable_areas
+        # set the item_location
         item_location = room.item_location
-        areas = room.searchable_areas
-        print("You might find something here:\n")
-        print(*areas, sep=" \t")
 
-        # ask user where to look
-        place_to_look = input("Where would you like to search?:\n")
-        place_to_look = place_to_look.upper()
+        # !!!!!!! Debug. Delete later
+        print(item_location)
+        # Give the user the list of searchable areas
+        print("These seem like worthy locations:\n")
+        for area in possible_areas:
+            print(area)
 
-        if place_to_look in areas:
-            # give user feedback
-            print(f"You search the {place_to_look}...\n")
-            sleep(2)
-            # checking if the user is searching the item_location
-            if place_to_look == item_location and not room.item_found:
-                item = room.inventory
-                item = item.upper()
-                print(f"You found the {item}\n")
-                room.item_found = True
-                # Calling the pick_up_item method to add the item to inventory
-                self.pick_up_item(item)
-            else:
-                print("You found nothing\n")
-                # repeating the method
-                # issue: the item_location is randomized each time
-                self.search(self.current_room,
-                            self.current_room.searchable_areas)
-        elif place_to_look == "EXIT":
-                return
+        # Prompt user to choose from the list    
+        player_choice = input("Where would you like to search?:\n")
+
+        # Validate user input
+        if self.validate_input(
+            player_choice,
+            f"Surely you understand that it is impossible to search a {player_choice}. Try using letters.",
+            "I need at least 2 letters to understand where you're looking."
+            ) == False:
+            self.search()
+
+        # If valid set to uppercase to match against searchable areas
+        player_choice = player_choice.upper()
+
+        # Cycle through each area
+        for x in possible_areas:
+            # Check for a partial string match of the area
+            if player_choice in x:
+                # Set player_choice to the full string
+                player_choice = x
+                # Give user feedback
+                print(f"You search the {x}")
+                sleep(2)
+        # Check if player has found the item and if item has not been found
+        if player_choice == item_location and not room.item_found:
+            # Set room.item_found to True
+            room.item_found == True
+            # Give user feedback
+            print(f"You found the {room.inventory.upper()}")
+            # Call the pick_up_item method
+            self.pick_up_item(room.inventory.upper())
         else:
-            print("That is not an area. Try again\n")
-            self.search(room, areas)
+            print(f"you found nothing")
+            # Repeats the method
+            self.search()
 
     def check_inventory(self):
         print(f"You are currently holding:\n {self.inventory}\n")
 
-    def look(self, room):
-        room = self.current_room
-        room.describe_paths()
+    def look(self):
+        pass
 
     def pick_up_item(self, new_item):
         """
@@ -185,26 +208,28 @@ class Player:
         If it exists, it will return the item.
         """
         if len(inventory) > 0:
+            print("You're currently holding:\n")
             print(*inventory, sep= ", ")
             chosen_item = input("What would you like to use?:\n")
             # Validating user input
             if self.validate_input(
                 chosen_item, 
-                "Not a valid selection. Please try again!",
-                "Not a valid selection. Please try again!",
+                f"It's hard to carry a {chosen_item}, perhaps try something real!",
+                f"I need more that {len(chosen_item)} to work with. Give me at least 3 letters",
                 ) == False:
                 return
             else:
                 # If valid
                 chosen_item = chosen_item.upper()
-                if chosen_item in self.inventory:
-                    print(f"You use the {chosen_item}\n")
+                for x in self.inventory:
+                    continue
+                if chosen_item in x:
+                    print(f"You use the {x}")
                 elif chosen_item == "EXIT":
                     return
                 else:
                     print("That's not in your inventory\n")
                     self.use_item(self.inventory)
-                return chosen_item
         else:
             print("You haven't got anything to use.\n")
 
@@ -222,9 +247,11 @@ class Player:
             "Sorry, that is not a valid direction\n"
             ) == False:
             # Repeating method until valid input is given
-            self.move_to_room(self.current_room, " ")
+            self.move_to_room(self.current_room, direction)
 
         direction = direction.lower()
+        if direction == "exit":
+            return
         # referencing the current rooms posible paths
         links = current_room.linked_rooms
         # checking if the user has chosen a possible cardinal direction
@@ -235,11 +262,11 @@ class Player:
             new_name = links[direction]
             # Calling the generate_room_from_name function to return the associated room object
             new_room = map.generate_room_from_name(new_name)
-            if new_room.required_item in self.inventory or new_room.has_event:
+            # Checking if user has the required item to move on
+            if new_room.required_item.upper() in self.inventory or new_room.has_event:
                 # Setting the player's current room to the new_room
                 self.current_room = new_room
                 # Describing the new room to the user
-                os.system('clear')
                 new_room.describe_room()
                 if new_room.has_event:
                     self.trigger_event(new_room, new_room.required_item)
@@ -254,10 +281,12 @@ class Player:
         room = self.current_room
         required_item = room.required_item
         used_item = self.use_item(self.inventory)
-        
+        used_item = used_item.upper()
         # Check for an encounter
         if room.has_encounter:
-            if used_item == required_item:
+            if used_item == required_item.upper():
+                print("...")
+                sleep(2)
                 print("You did it\n")
                 room.has_encounter = False
                 room.has_event = False
@@ -265,11 +294,14 @@ class Player:
                 print("You Died. Game Over\n")
                 self.alive = False
         else:
-            if used_item == required_item:
+            if used_item == required_item.upper():
+                print("...")
+                sleep(2)
                 print("You did it\n")
                 room.has_encounter = False
                 room.has_event = False
             else:
+                print("...")
                 sleep(2)
                 print("It did not work\n")
                 self.trigger_event(room, required_item)
