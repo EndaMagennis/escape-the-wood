8bfbb3724b32029fa5aede882e4ad9ea0ebfb5b9
Enda Magennis <101476399+RopesMcMeme@users.noreply.github.com>
2023-08-25 20:23:16 +0100

    Create function to clear console depending on shell

--- game_environment.py
+++ game_environment.py
@@ -1,4 +1,17 @@
+from os import system, name
+
 class GameEnvironment:
     """
     Handles game state to allow for save/load functionality
     """
+
+
+def clear_terminal():
+    """ Function which check the shell and uses the relevant clear command"""
+    # Check if operating system name is Windows
+    if name == 'nt':
+        command = system('cls')
+ 
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        command = system('clear')
