7eb7b3fc289aa0d05251f654020727c5efb64dd7
Enda Magennis <101476399+RopesMcMeme@users.noreply.github.com>
2023-08-26 12:10:35 +0100

    Update item descriptions and create return_item_description function

--- item.py
+++ item.py
@@ -1,11 +1,10 @@
 FINDABLE_ITEMS = {
     # dictionary entries for types of items which can be found in the game
     "weapons": {
-        "broken sword": 10,
-        "shortsword": 30,
-        "dagger": 25,
-        "mace": 35,
-        "bow": 35,
+        "broken sword": "Useless as a weapon, but the edge could hack a few branches!",
+        "shortsword": "Well balanced and swift, this blade will serve you well!",
+        "mace": "A brutal weapon, but great for taking care of wild beasts",
+        "bow": "Great for enemies at a distance!",
     },
     "keys": {
         "cottage key": "key to the cottage",
@@ -17,9 +16,9 @@ FINDABLE_ITEMS = {
         "book": "A Teological examination of the Old Gods",
     },
     "equipment": {
-        "torch": 10,
-        "rope": "50 feet",
-        "arrows": 10
+        "torch": "Already oiled, and with a striker to boot!",
+        "rope": "50 feets worth, I reckon",
+        "arrows": "A bundle of 10 arrows"
     }
 }
 
@@ -33,5 +32,11 @@ def return_item(request):
             if request == in_key:
                 return in_key
 
-
-
+def return_item_description(request):
+    
+    """A funtion to return an item descrition"""
+    for out_key, out_value in FINDABLE_ITEMS.items():
+        for in_key, in_value in out_value.items():
+            if request == in_key:
+                return in_value
+            
