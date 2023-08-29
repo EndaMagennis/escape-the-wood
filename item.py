b9930e4c771b459a73e8e11653581c1db9f61bc5
Enda Magennis <101476399+RopesMcMeme@users.noreply.github.com>
2023-08-29 13:42:03 +0100

    Fix: formatting and validation

--- item.py
+++ item.py
@@ -1,21 +1,33 @@
 FINDABLE_ITEMS = {
     # dictionary entries for types of items which can be found in the game
     "weapons": {
-        "broken sword": "Useless as a weapon, but the edge could hack a few branches!",
-        "shortsword": "Well balanced, sharp, and swift, this blade will serve you well!",
-        "mace": "A brutal weapon, but great for taking care of wild beasts",
-        "bow": "Great for enemies at a distance!",
+        "broken sword": f"""
+        Useless as a weapon, but the edge could hack a few branches!
+        """,
+        "shortsword": f"""
+        Well balanced, sharp, and swift, this blade will serve you well!
+        """,
+        "mace": f"""
+        A brutal weapon, but great for taking care of wild beasts
+        """,
+        "bow": f"""
+        Great for enemies at a distance!
+        """,
     },
     "keys": {
-        "cottage key": "Key to the cottage",
-        "rusted key": "An old key, brittle and rough"
+        "cottage key": f"""
+        Key to the cottage
+        """,
+        "rusted key": f"""
+        An old key, brittle and rough
+        """
     },
     "notes": {
         "book": f"""
             Theobald Bantreck's Rules for Survival:
 
         Always be prepared to meet a goblin by flowing water
-        They have been known to ransack unsuspecting people by rivers. 
+        They have been known to ransack unsuspecting people by rivers.
         A sharp blade is usually more than enough to deal with them.
         """,
     },
@@ -35,11 +47,11 @@ def return_item(request):
             if request == in_key:
                 return in_key
 
+
 def return_item_description(request):
-    
+
     """A funtion to return an item descrition"""
     for out_key, out_value in FINDABLE_ITEMS.items():
         for in_key, in_value in out_value.items():
             if request == in_key:
                 return in_value
-            
