7d8768013ae10243c5f382ff329d815f938ae44d
Enda Magennis <101476399+RopesMcMeme@users.noreply.github.com>
2023-08-29 14:06:29 +0100

    Refactor and validate

--- run.py
+++ run.py
@@ -1,74 +1,9 @@
-import player
-import map
 import game_environment
-import os
-import sys
-from time import sleep
-from colorama import Fore
-
-# The main logo in ascii
-logo ="""
-    ______                         
-   / __________________ _____  ___ 
-  / __/ / ___/ ___/ __ `/ __ \/ _ \/
- / /___(__  / /__/ /_/ / /_/ /  __/
-/_____/____/\___/\__,_/ .___/\___/ 
-                     /_/           
-           ________       
-          /_  __/ /_  ___ 
-           / / / __ \/ _ \/
-          / / / / / /  __/
-         /_/ /_/ /_/\___/ 
- _       __                __    
-| |     / ____  ____  ____/ _____
-| | /| / / __ \/ __ \/ __  / ___/
-| |/ |/ / /_/ / /_/ / /_/ (__  ) 
-|__/|__/\____/\____/\__,_/____/  
-
-"""
-
-# Variable to store the intro of the game
-intro = f"""
-    You have awoken in a ramshackel abode on the outskirts of a town.
-    You have no recollection of who you are or how you've gotten here.
-    A voice calls out to you, seemingly from nowhere.
-    
-        'Ho, there weary traveller, you are here for the Challenge of Kings.
-    That you have awoken is a testament in itself.
-    Now piece yourself together. Who are you?'
-
-    Your head begins swimming; the past rushing to meet the present.
-    You suddenely remember yourself.
-    """
 
 
 def main():
-    # Turning off user inputs
-    os.system("stty -echo")
-    # Printing logo
-    print(Fore.CYAN + logo)
-    # Waiting 2 seconds
-    sleep(2)
-    # Clearing the terminal
-    game_environment.clear_terminal()
-    # Typing the intro
-    for char in intro:
-        sleep(0.02)
-        sys.stdout.write(Fore.GREEN + char)
-        sys.stdout.flush()
-    # Turning on user inputs
-    os.system("stty echo")
-    # Setting item_locations
-    map.set_item_locations_for_each_room()
-    # Initializing rooms and enemies lists
-    game_environment.check_for_win_state()
-    # Calling player.Player.name_player method
-    player.current_player.name_player()
-    # Describing current room
-    player.current_player.current_room.describe_room()
-    # Calling the check_for_win_state function to initalize lists
-    player.current_player.register_user_inputs()
-    
+    """Calls to initalize the game"""
+    game_environment.initialize_game()
 
 
 if __name__ == '__main__':
